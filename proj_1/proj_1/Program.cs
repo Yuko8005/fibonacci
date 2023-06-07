a58350e7b1c6d0bc7dd80cccec93afda61b40809
Yuko8005 <yukoko6789@gmail.com>
2023-06-07 11:02:58 +0300

    first_commit

---
+++ proj_1/proj_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 40;
+        PrintFibonacciNumbers(n);
+    }
+
+    static void PrintFibonacciNumbers(int n)
+    {
+        int a = 0;
+        int b = 1;
+
+        Console.WriteLine("Перші {0} чисел Фібоначчі:", n);
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+        for (int i = 2; i < n; i++)
+        {
+            int c = a + b;
+            Console.WriteLine(c);
+            a = b;
+            b = c;
+        }
+    }
+}
